9442760c8d3ba1f6384403cc5f25579e4c2760da
ANIL KUMAR VERMA <98508958+Anil2784@users.noreply.github.com>
2024-01-05 12:12:42 +0530

    Pop

---
+++ index.js
@@ -0,0 +1,17 @@
+let popup = document.getElementById("popup");
+
+// let open=(() =>{
+//     popup.classList.add("openPopup");
+// });
+// console.log(open)
+// let close=(() =>{
+//     popup.classList.remove("closePopup");
+// })
+
+function openPopup(){
+   popup.classList.add("open-popup"); 
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup"); 
+ }
